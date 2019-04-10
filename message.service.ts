2bf5688cc211a6ac255d06f64882f23f6d0bf776
training <training@STUDENT.IBAT>
2019-04-10 21:19:28 +0100

    firstweekangular

---
+++ message.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+messages: string[] = ['slkdjsdljf','ldksjdslkfjdsf']
+  constructor() { }
+  add(message:string){this.messages.push(message);}
+  clear(){this.messages =[]}
+}
